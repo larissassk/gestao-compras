98a2c30217c04c1458ee74849d79ebcfe7effaa3
larissassk <larissasantos200702@gmail.com>
2024-07-29 19:16:50 -0300

    atualiza estilo

--- marca.php
+++ marca.php
@@ -6,6 +6,37 @@
     <title>Cadastrar Marca</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+<style>
+    
+label {
+    margin-bottom: 8px;
+    color: #333;
+    font-weight: bold;
+}
+
+input[type="text"] {
+    padding: 10px;
+    margin-bottom: 20px;
+    border: 1px solid #e0e0e0;
+    border-radius: 4px;
+    font-size: 16px;
+}
+
+input[type="submit"] {
+    padding: 10px;
+    background: #738af0; /* Cor do botão */
+    color: #ffffff;
+    border: none;
+    border-radius: 4px;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background 0.3s ease;
+}
+
+input[type="submit"]:hover {
+    background: #4a60d1; /* Efeito hover no botão */
+}
+</style>
 <body>
     <header>
         <div class="center">
